fb318b7292a45710bdf1bfcf5022c75579bed99f
dawit <dawitkebedewoldu@gmail.com>
2024-05-01 00:40:34 +0300

    first commit

--- library_management/__init__.py
+++ library_management/__init__.py
@@ -1,1 +1,7 @@
 __version__ = "0.0.1"
+# from frappe. import add_website_route
+
+# def add_custom_homepage():
+#   add_website_route("/", "custom_app.controllers.index")
+
+# add_website_route_hook = add_custom_homepage
